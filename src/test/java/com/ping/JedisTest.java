688f57b2ba021b0c2060cb116cdabce863f0683e
liyaoping <liyaoping@qq.com>
2020-03-17 23:01:55 +0800

    操作数据类型list

--- src/test/java/com/ping/JedisTest.java
+++ src/test/java/com/ping/JedisTest.java
@@ -1,18 +1,41 @@
 package com.ping;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import redis.clients.jedis.Jedis;
 
 public class JedisTest {
 
+	/**
+	 * 1.数据类型string
+	 */
 	@Test
-	public void testJedis() {
+	public void testJedisString() {
 		//1.连接redis
 		Jedis jedis = new Jedis("127.0.0.1",6379);
 		//2.操作redis
 //		jedis.set("name", "liyaoping");
 		System.out.println(jedis.get("name"));
+		//3.关闭redis
+		jedis.close();
+	}
+	
+	/**
+	 * 2.数据类型list
+	 */
+	@Test
+	public void testJedisList() {
+		//1.连接redis
+		Jedis jedis = new Jedis("127.0.0.1",6379);
+		//2.操作redis
+		jedis.lpush("list1", "a","b","c");
+		jedis.rpush("list1", "d");
+		List<String> lrange = jedis.lrange("list1", 0, -1);
+		for (int i = 0; i < lrange.size(); i++) {
+			System.out.println(lrange.get(i));
+		}
 		//3.关闭redis
 		jedis.close();
 	}
